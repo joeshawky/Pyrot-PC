751eb56a6b8a1ecf4273fa2cf6a28688c0da6690
joeshawky <youssefmetawe0@gmail.com>
2023-11-24 19:37:07 +0300

    Created shell script to update libraries

---
+++ update_dependencies.sh
@@ -0,0 +1,10 @@
+# Initialize and update the first submodule
+git submodule init
+git submodule update
+
+# Navigate to the specified directory
+cd libs/libevents/libevents/libs/cpp/parse
+
+# Initialize and update the second submodule
+git submodule init
+git submodule update
